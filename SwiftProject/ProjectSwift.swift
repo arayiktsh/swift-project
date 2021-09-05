4cba71acc9d3079de530d25c3074d906a0bb4822
Arayik-Tshagharyan <arayik.t.y@gmail.com>
2021-09-05 13:28:35 +0400

    tumoSwiftProject

---
+++ SwiftProject/ProjectSwift.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectSwift.swift
+//  SwiftProject
+//
+//  Created by user on 7/26/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+extension CGImagePropertyOrientation {
+    init(_ uiImageOrientation: UIImage.Orientation) {
+        switch uiImageOrientation {
+        case .up: self = .up
+        case .down: self = .down
+        case .left: self = .left
+        case .right: self = .right
+        case .upMirrored: self = .upMirrored
+        case .downMirrored: self = .downMirrored
+        case .leftMirrored: self = .leftMirrored
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
